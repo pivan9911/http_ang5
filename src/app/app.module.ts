ed5d2f66f2b88521640ac9147815038aa4443fdc
pivan9911 <pivan9911@gmail.com>
2017-12-06 16:37:55 +0100

    as ng-book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { MoreHttpRequestsComponent } from './more-http-requests/more-http-requests.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
+import { SearchResultComponent } from './you-tube-search/search-result.component';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent,
+    MoreHttpRequestsComponent,
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule // <-- right here
   ],
-  providers: [],
+  providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
